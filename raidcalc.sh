8af1c493da3038f79fb3ca4f23237bebca5d1557
Robin Cerny <robin@cerny.li>
2015-03-23 18:55:01 +0100

    added the first "calc" part

--- raidcalc.sh
+++ raidcalc.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-read -p "Raidtype: " RTYPE
-echo "Raidtype: $RTYPE"
-read -p "Number of Disks: " NDISKS
-echo "Number disks: $NDISKS"
-read -p "Single disk size in TB: " DSIZE
-echo "Disk size: $DSIZE"
+raid_0() {
+	CAPACITY=$(echo "scale=3;$NDISKS*$DSIZE" | bc | | sed "s/^\(-\)\?\./\10./")
+	echo "$CAPACITY"
+}
+
+raid_1() {
+	echo raid1
+	CAPACITY="$DSIZE"
+}
+
+raid_5() {
+	echo raid5
+	NDISKS=3
+	DSIZE="1.5"
+	CAPACITY=$(echo "scale=3;$DSIZE*($NDISKS-1)" | bc | sed "s/^\(-\)\?\./\10./")
+	echo "$CAPACITY"
+}
+
+raid_5
+
+raid_5E() {
+	echo raid5e
+}
+
+raid_5EE() {
+	echo raid5ee
+}
+
+raid_6() {
+	echo raid6
+}
+
+raid_10() {
+	echo raid10
+}
+
+exit 255
 
 case "$RTYPE" in
 	0)
@@ -27,3 +58,12 @@ case "$RTYPE" in
 		echo "Not a valid RAID level."
 	;;
 esac
+
+read -p "Raidtype: " RTYPE
+echo "Raidtype: $RTYPE"
+read -p "Number of Disks: " NDISKS
+echo "Number disks: $NDISKS"
+read -p "Single disk size in TB: " DSIZE
+echo "Disk size: $DSIZE"
+
+
