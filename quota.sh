c215dfd0c687ef449d58d7425c618fe832e2471a
Robin Cerny <robin@cerny.li>
2015-04-01 00:16:08 +0200

    added a warning function

--- quota.sh
+++ quota.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # userquota parsescript by tschaerni <robin@cerny.li>
 # release date: 31.03.2015 or for the illiberal users: 2015-03-31
-# version: 1.0.0
+# version: 1.1.0
+#
+# Changelog:
+#
+#	1.1.0 - added a warning if freequota lower then 1 MB
+#
+#	1.0.0 - release
 #
 # Important note:
 #	The current version of this script works
@@ -12,14 +18,14 @@ GETQUOTA="$(quota -lw --hide-device | tail -1)"
 USERQUOTA="$(echo "$GETQUOTA" | awk {'print $2'})"
 USERAMOUNT="$(echo "$GETQUOTA" | awk {'print $1'})"
 FREEQUOTA="$(( USERQUOTA-USERAMOUNT ))"
+
 if [ "$FREEQUOTA" -lt "1000" ]
 then
-	FREEQUOTA="$FREEQUOTA KB"
+	echo -e "$FREEQUOTA KB \e[31mWarning:\e[0m your quota is almost reached!"
 elif [ "$FREEQUOTA" -lt "1000000" ]
 then
-	FREEQUOTA="${FREEQUOTA::-3}.${FREEQUOTA: -3:1} MB"
+	echo "${FREEQUOTA::-3}.${FREEQUOTA: -3:1} MB"
 else
-	FREEQUOTA="${FREEQUOTA::-6}.${FREEQUOTA: -6:1} GB"
+	echo "${FREEQUOTA::-6}.${FREEQUOTA: -6:1} GB"
 fi
-echo "$FREEQUOTA"
 exit 0
