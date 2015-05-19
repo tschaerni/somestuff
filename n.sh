df5489151ee3b6dde7c87cf9cca04626fcd1aa87
Robin Cerny <robin@cerny.li>
2015-05-19 15:33:00 +0200

    moved the symlinked script n.sh

--- n.sh
+++ n.sh
@@ -1,1 +1,65 @@
-/home/rcerny/.n.sh
+#!/usr/bin/zsh
+function n() {
+	if [[ "$#" = "0" ]]
+	then
+		echo "Usage:         n <name of note>"
+		echo "list notes:    n -ls"
+		echo "remove notes:  n -rm or -del or --delete"
+	else
+		case "$1" in
+		"-ls")
+			shift
+			if [[ -z "$1" ]]
+			then
+				ls -lt ~/notes/
+			else
+				ls -lt ~/notes/*$**
+			fi
+		;;
+		"-rm"|"-del"|"--delete")
+			shift
+			if ls ~/notes/*$** &> /dev/null
+			then
+				NUMNOTES=$(ls -l ~/notes/*$** | wc -l)
+				if [[ "$NUMNOTES" > "1" ]]
+				then
+					echo "there are more then one file with the given name:"
+					echo ""
+					ls -lt ~/notes/*$**
+					echo ""
+					echo "please specify"
+				else
+					NOTE=$(ls ~/notes/*$**)
+					read "NOTEDEL?delete $NOTE ? [y/N]: "
+					if [[ "${(L)NOTEDEL}" = "y" || "${(L)NOTEDEL}" = "yes" ]]
+					then
+						rm -v "$NOTE"
+						echo "deleted"
+					else
+						echo "abort"
+					fi
+				fi
+			fi
+		;;
+		*)
+			if ls ~/notes/*$** &> /dev/null
+			then
+				NUMNOTES=$(ls -l ~/notes/*$** | wc -l)
+				if [[ "$NUMNOTES" > "1" ]]
+				then
+					echo "there are more then one file with the given name:"
+					echo ""
+					ls -lt ~/notes/*$**
+					echo ""
+					echo "please specify"
+				else
+					NOTE=$(ls ~/notes/*$**)
+					$EDITOR $NOTE
+				fi
+			else
+				$EDITOR ~/notes/"$*".txt
+			fi
+		;;
+		esac
+	fi
+}
